121a1c24f25b34985a9d113350bb8350368cd295
laura <laura@lauramackenzie.net>
2017-01-13 00:33:52 -0500

    updated truncate.js

--- truncate.js
+++ truncate.js
@@ -1,21 +1,3 @@
-// This is the file where you will write the Truncate Words function and related code.
-
-// TODO: Create a Function called truncateWords() that accepts two arguments: 
-// longText (a String with several words in it)
-// numWords (an Integer that sets the number of words you want in the returned text)
-
-// TODO: Within the truncateWords() Function, complete the following steps:
-// 1. Use the split() function to split the String into an Array
-// 2. Use the length attribute to find the number of words in the Array
-// 3. Determine how many words should be removed from the String
-// 4. Remove those words from the Array
-// 5. Add an additional String item to the Array to put an ellipses in: "..."
-// 6. Use the join() function to convert the Array back into a String
-// 7. Return the truncated String from the Function
-
-// This portion of the script is meant to call and display the result of your Function.
-// You do not need to change the following lines, but you may change them if you 
-// are experimenting further or pursuing stretch goals.
 //invoke strict mode
 "use strict";
 //full string 
